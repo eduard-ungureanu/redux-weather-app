c0db784971567580d09d37f63893dad9d02be2c7
Eduard Ungureanu <u.eduard.email@gmail.com>
2018-10-07 01:11:21 +0300

    inserting charts

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 
+import WeatherLit from '../containers/weather_list';
+
 export default class App extends Component {
   render() {
     return (
       <div className="app-wrapper">
         <SearchBar/>
+        <WeatherLit/>
       </div>
     );
   }
---
+++ src/containers/weather_list.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+   renderWeahter(cityData) {
+      const name = cityData.city.name;
+      const temps = cityData.list.map(weather => weather.main.temp);
+      const pressures = cityData.list.map(weather => weather.main.pressure);
+      const humidities = cityData.list.map(weather => weather.main.humidity);
+
+      return (
+         <tr key={name}>
+            <td>{name}</td>
+            <td><Chart data={temps} color="orange" units="K"/></td>
+            <td><Chart data={pressures} color="green"  units="hPa"/></td>
+            <td><Chart data={humidities} color="blue"  units="%"/></td>
+         </tr>
+      )
+   }
+   render() {
+      return (
+         <table className="table table-hover">
+            <thead>
+               <tr>
+                  <th>City</th>
+                  <th>Temperature (k)</th>
+                  <th>Pressure (hPa)</th>
+                  <th>Humidity %</th>
+               </tr>
+            </thead>
+            <tbody>
+               {this.props.weather.map(this.renderWeahter)}
+            </tbody>
+         </table>
+      )
+   }
+}
+
+function mapStateToProps({weather}) {
+   return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
